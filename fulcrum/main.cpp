afa1e02b8e740f1ed6e87580fb561978580129f0
wpichl <walter.pichl@hotmail.com>
2022-04-04 17:09:55 +0200

    Initial Commit

---
+++ fulcrum/main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line, int cmd_show)
+{
+	return 0;
+}
