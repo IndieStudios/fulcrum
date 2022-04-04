4b9390d83b8858e927dfc022ceacaf3fc5a2b88b
wpichl <walter.pichl@hotmail.com>
2022-04-04 17:59:38 +0200

    Add engine to display a window

--- fulcrum/main.cpp
+++ fulcrum/main.cpp
@@ -1,9 +1,13 @@
 #include <Windows.h>
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "engine/engine.hpp"
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line, int cmd_show)
 {
+	if (!engine::get().initialize(800, 600, "fulcrum"))
+	{
+		return 1;
+	}
+	engine::get().run();
 	return 0;
 }
